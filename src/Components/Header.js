cc47797318a119b58de0009cf095395508c43d9a
Thiago Ferreira <thiagocristhian@gmail.com>
2021-04-12 19:54:09 -0300

    Nova página no git hub pages

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,16 @@
 import React from 'react';
-//test
 
 function Header() {
   return (
       <header className="masthead">
-        <div className="inner fixed-top" style={{ marginTop: '1%'}}>
-          <nav className="nav nav-masthead">
-            <div>
-              <button type="button" className="btn btn-outline-success mr-3 ml-3"><a className="nav-link h3 active" href="/">Home</a></button>
-              <button type="button" className="btn btn-outline-success mr-3 ml-3"><a className="nav-link h3 active" href="/sobre">Sobre</a></button>
-              <button type="button" className="btn btn-outline-success mr-3 ml-3"><a className="nav-link h3 active" href="/habilidades">Habilidades</a></button>
-              <button type="button" className="btn btn-outline-success mr-3 ml-3"><a className="nav-link h3 active" href="/projetos">Projetos</a></button>
-            </div>
-          </nav>
-        </div>
+        <nav className="nav nav-masthead inner fixed-top" style={{marginTop: '1%'}}>
+          <div style={{marginLeft: 'auto', marginRight: 'auto'}}>
+            <button type="button" className="btn btn-outline-success mr-3 ml-3"><a className="nav-link h3 active" href="/">Home</a></button>
+            <button type="button" className="btn btn-outline-success mr-3 ml-3"><a className="nav-link h3 active" href="/sobre">Sobre</a></button>
+            <button type="button" className="btn btn-outline-success mr-3 ml-3"><a className="nav-link h3 active" href="/habilidades">Habilidades</a></button>
+            <button type="button" className="btn btn-outline-success mr-3 ml-3"><a className="nav-link h3 active" href="/projetos">Projetos</a></button>
+          </div>
+        </nav>
       </header>
   )
 }
