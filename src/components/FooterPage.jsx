f04d523832b46350f6a5d2ff752c89d429d0e49f
Thiago Cristhian <thiago.cristhian@alloha.com>
2025-04-22 23:02:13 -0300

    Fix: Ajustando links

--- src/components/FooterPage.jsx
+++ src/components/FooterPage.jsx
@@ -5,7 +5,7 @@ function FooterPage() {
         <div className="fixed bottom-8 w-full z-50">
             <div className="flex flex-wrap gap-8 justify-center text-3xl">
                 <a
-                    href="https://wa.me/seu-numero"
+                    href="https://wa.me/5516992923943"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-green-500 hover:text-green-600 transition-transform transform hover:scale-150
@@ -15,7 +15,7 @@ function FooterPage() {
                     <FaWhatsapp />
                 </a>
                 <a
-                    href="https://linkedin.com/in/seu-linkedin"
+                    href="https://www.linkedin.com/in/thiago-c-ferreira/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-blue-600 hover:text-blue-700 transition-transform transform hover:scale-150
@@ -25,7 +25,7 @@ function FooterPage() {
                     <FaLinkedin />
                 </a>
                 <a
-                    href="https://github.com/seu-github"
+                    href="https://github.com/thiagocristhianferreira"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-800 hover:text-black transition-transform transform hover:scale-150
@@ -35,7 +35,7 @@ function FooterPage() {
                     <FaGithub />
                 </a>
                 <a
-                    href="mailto:seu-email@gmail.com"
+                    href="mailto:thiagocristhian@gmail.com"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-red-500 hover:text-red-600 transition-transform transform hover:scale-150
